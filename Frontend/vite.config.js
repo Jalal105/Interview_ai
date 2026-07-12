962ed0dbacebe8d64e7c3fe296b86201579ec90f
SK Jalaludiin <your.email@example.com>
2026-07-12 10:53:41 +0530

    fix: allow all hosts in vite preview and server config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: true,
+  },
   preview: {
     host: '0.0.0.0',
     port: 10000,
-    allowedHosts: ['interview-ai-5.onrender.com', 'all'],
+    allowedHosts: true,
   },
 })
